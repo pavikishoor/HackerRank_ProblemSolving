a53ba0642da1200e00c317b02b9f667439f05797
pavikishoor <31266604+pavikishoor@users.noreply.github.com>
2020-07-29 11:43:24 +0530

    Update 2_Simple_Array_Sum.py

--- 2_Simple_Array_Sum.py
+++ 2_Simple_Array_Sum.py
@@ -1,12 +1,13 @@
+#!/bin/python3
+
 import os
 import sys
 
+#
+# Complete the simpleArraySum function below.
+#
 def simpleArraySum(ar):
-    sum=0;
-    for i in range(len(ar)):
-        sum+=ar[i]
-    return (sum)
-
+   return sum(ar)
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
